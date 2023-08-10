a5c1a9f65cfc67473422883e921f44a5b0871d5d
M. Washington <mickaell.washington@hotmail.com>
2023-08-09 21:26:16 -0300

    Estudo de Módulos
    Aprendi a separar funções por módulos

---
+++ Módulos/calc.js
@@ -0,0 +1,9 @@
+let somaFunc = require('./Funções/somar')
+let subFunc = require('./Funções/sub')
+let divFunc = require('./Funções/div')
+let multiFunc = require('./Funções/multi')
+
+console.log('O resultado da adição é: ' + somaFunc(10,5));
+console.log('O resultado da subtração é: ' + subFunc(10,5));
+console.log('O resultado da divisão é: ' + divFunc(10,5));
+console.log('O resultado da multiplicação é: ' + multiFunc(10,5));
