c216eb4c255f41aeabb95bdd75ec2269a0be5be3
M. Washington <mickaell.washington@hotmail.com>
2023-08-10 20:34:25 -0300

    Handlebars e Criação de Formulário
    Aprendi a instalar o Handlebars que é uma Template Engine e criei uma formulário, para pôr em prática o que aprendi com rotas.

---
+++ #11 Handlebars/README.md
@@ -0,0 +1,3 @@
+Handlebars é template engine, que dá muitas funcionabilidades pro HTML, como estruturas condicionais e exibir dados do back end no HTML.
+
+npm i --save express-handlebars
---
+++ #11 Handlebars/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+
+// Config  
+    // Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+    // Conexão com o banco de dados MySQL
+        const sequelize = new Sequelize('testenodejs', 'root', 'mwdev', {
+        host: 'localhost',
+        dialect: 'mysql'
+        });
+
+// Rotas
+        app.get('/cad', function(req, res){
+            res.render('formulario');
+        });
+
+
+
+
+
+app.listen(8081, function(){
+    console.log('Servidor ON!')
+})
